d792b4afb3969a2825f83269545694bf83d56556
sazidahossain <sazidahossain@gmail.com>
2018-11-01 11:21:31 +0300

    Task submission

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import authors from "./data.js";
-
 // Components
 import Sidebar from "./Sidebar";
 import SearchBar from "./SearchBar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
-
+import Loading from "./Loading";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentAuthor: {},
-      filteredAuthors: []
+      filteredAuthors: [],
+      authors: [],
+      loading: false
     };
     this.selectAuthor = this.selectAuthor.bind(this);
     this.unselectAuthor = this.unselectAuthor.bind(this);
     this.filterAuthors = this.filterAuthors.bind(this);
   }
-
+  componentDidMount() {
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(response => response.data)
+      .then(authors => this.setState({ authors: authors, loading: true }))
+      .catch(error => console.error(error));
+  }
   selectAuthor(author) {
-    this.setState({ currentAuthor: author });
+    console.log(author);
+    axios
+      .get(`https://the-index-api.herokuapp.com/api/authors/${author.id}/`)
+      .then(response => response.data)
+      .then(author => this.setState({ currentAuthor: author, loading: true }))
+      .catch(error => console.error(error));
   }
 
   unselectAuthor() {
@@ -31,24 +42,33 @@ class App extends Component {
 
   filterAuthors(query) {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`.includes(query);
     });
     this.setState({ filteredAuthors: filteredAuthors });
   }
 
   getContentView() {
-    if (this.state.currentAuthor.first_name) {
-      return <AuthorDetail author={this.state.currentAuthor} />;
-    } else if (this.state.filteredAuthors[0]) {
-      return (
-        <AuthorsList
-          authors={this.state.filteredAuthors}
-          selectAuthor={this.selectAuthor}
-        />
-      );
+    if (this.state.loading) {
+      if (this.state.currentAuthor.first_name) {
+        return <AuthorDetail author={this.state.currentAuthor} />;
+      } else if (this.state.filteredAuthors[0]) {
+        return (
+          <AuthorsList
+            authors={this.state.filteredAuthors}
+            selectAuthor={this.selectAuthor}
+          />
+        );
+      } else {
+        return (
+          <AuthorsList
+            authors={this.state.authors}
+            selectAuthor={this.selectAuthor}
+          />
+        );
+      }
     } else {
-      return <AuthorsList authors={authors} selectAuthor={this.selectAuthor} />;
+      return <Loading />;
     }
   }
 
